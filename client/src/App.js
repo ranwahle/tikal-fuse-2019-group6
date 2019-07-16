990a07439c8687ca62199c6143697bcb8edb4d4a
ranwahle <ran.wahle@gmail.com>
2019-07-16 16:37:56 +0300

    Run experiments

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import logo from './MyChaoticHeart.jpg';
 import './App.css';
-import getExperiments from "./ExperimentsService";
+import getCurrentExperiments, {getExperiments, runExperiment} from "./ExperimentsService";
 
 class App extends React.Component {
 
      componentDidMount() {
-        this.setState({experiments: []}, async () => {
-            const currentExperiments = await getExperiments();
+        this.setState({currentExperiments: []}, async () => {
+            const currentExperiments = await getCurrentExperiments();
             console.log('curent exp', currentExperiments);
-            this.setState({experiments: currentExperiments});
+            this.setState({currentExperiments: currentExperiments}
+            ,async () => {
+                const experiments = await getExperiments();
+                this.setState({experiments: experiments})
+                }) ;
         });
     }
 
+    runExperiment = (event) => {
+         const {value} = event.currentTarget;
+         runExperiment(value);
+    }
+
     render() {
-        const experiments = this.state ? this.state.experiments : {};
-        const active = experiments.active || {};
+        const currentExperiments = this.state ? this.state.currentExperiments : {};
+        const experiments = this.state && this.state.experiments ? this.state.experiments : [];
+        const active = currentExperiments.active || {};
         console.log('active', active);
         return (
             <div className="App">
+                <select onChange={this.runExperiment}>
+                    {experiments.map(experiment =>
+                        (<option key={experiment.name} value={experiment.name}>{experiment.description}
+                        </option>))
+                    }
+                </select>
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
 
@@ -26,7 +42,17 @@ class App extends React.Component {
                 </header>
                 <div>
                     <h2>  Active: </h2>
-                    {Object.keys(active).map(key => (<li key={key}>{key}</li>))}
+                    {Object.keys(active).map(key => {
+                        const activeExperiments = active[key] || [];
+
+                        return (<li key={key}>{key}
+                            {
+                                activeExperiments.map(exp => {
+                                    return  (<span>Time: {new Date(exp.time_start)}</span>)
+                                })
+                            }
+                        </li>)
+                    })}
                 </div>
 
             </div>
--- client/src/ExperimentsService.js
+++ client/src/ExperimentsService.js
@@ -1,9 +1,20 @@
 import axios from "axios";
 
-async function getExperiments() {
+async function getCurrentExperiments() {
     const response = await axios.get('/api/experiments/status')
 
     return response.data;
 }
 
-export default getExperiments;
+export async function getExperiments() {
+    const response = await axios.get('/api/experiments')
+
+    return response.data.experiments;
+}
+
+export  function runExperiment(name) {
+    axios.get(`/api/experiments/${name}/run`);
+}
+
+
+export default getCurrentExperiments;
