0af043e172f0c8f8111028833a9222e7345eb582
Haim Cohen <hcloli@gmail.com>
2019-07-16 16:30:25 +0300

    Attack_cart experiment with POST and GET

--- server/experiments.py
+++ server/experiments.py
@@ -1,6 +1,7 @@
 from celery import Celery
 import time
 import requests
+import json
 
 worker = Celery('experiments',
                 backend='redis://localhost',
@@ -16,13 +17,25 @@ def dummy():
 
 @worker.task()
 def attack_cart():
+    # 03fef6ac-1896-4ce8-bd69-b798f85c6e0b
+
+    s = requests.Session()
+
     url = "http://g6.fuse.tikal.io/cart"
+    body = {
+        "id": "03fef6ac-1896-4ce8-bd69-b798f85c6e0b"
+    }
+    request_body = json.dumps(body)
+    headers = {'Content-Type': 'application/json; charset=UTF-8'}
+    print(request_body)
+    post_response = s.post(url=url, data=request_body, headers=headers)
+    print("Add to cart %s" % post_response)
 
     params = {}
 
-    r = requests.get(url=url, params=params)
+    r = s.get(url=url, params=params)
 
     data = r.json()
 
     # printing the output
-    print("Response: %s" % str(data))
+    print("Cart get response: %s" % str(data))
--- server/server.py
+++ server/server.py
@@ -3,11 +3,13 @@ from experiments import dummy, worker, attack_cart
 
 app = Flask(__name__)
 
-class Experiment():
+
+class Experiment:
 
     def __init__(self, task, desc):
         self.task = task
         self.desc = desc
+
 
 experiment_manifest = {
     'dummy': Experiment(dummy, 'A dummy task'),
@@ -36,20 +38,18 @@ Welcome to the fuze api server
 def inspect_experiments():
     i = worker.control.inspect()
     return {
-        #'registered': i.registered(),
-        'active': i.active(),
-        #'scheduled': i.scheduled(),
-        #'reserved': i.reserved(),
+        'active': i.active()
     }
+
 
 @app.route('/api/experiments')
 def list_experiments():
-    return { 'experiments': [ { 'name': k, 'description': v.desc } for k, v in
-            experiment_manifest.items() ] }
+    return {'experiments': [{'name': k, 'description': v.desc} for k, v in
+                            experiment_manifest.items()]}
+
 
 @app.route('/api/experiments/<name>/run')
 def run_experiments(name):
-
     experiment = experiment_manifest.get(name)
 
     if not experiment:
@@ -57,15 +57,18 @@ def run_experiments(name):
 
     experiment.task.delay()
 
-    return { 'result': 'ok' }
+    return {'result': 'ok'}
+
 
 @app.route('/api')
 def api_example():
     return {'this_is': {'a': 'json'}, 'file': 42}
 
+
 @app.route('/api/<method>')
 def api_method_example(method):
     return {'this_is': {'a': method}, 'file': 42}
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
