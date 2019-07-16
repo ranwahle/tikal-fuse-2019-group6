7e71e664577290239450d405345249271da1476a
Haim Cohen <hcloli@gmail.com>
2019-07-16 17:04:21 +0300

    Experiment cart attack use loadtest

--- server/experiments.py
+++ server/experiments.py
@@ -1,7 +1,8 @@
 from celery import Celery
 import time
-import requests
-import json
+import os
+# import requests
+# import json
 
 worker = Celery('experiments',
                 backend='redis://localhost',
@@ -18,24 +19,24 @@ def dummy():
 @worker.task()
 def attack_cart():
     # 03fef6ac-1896-4ce8-bd69-b798f85c6e0b
-
-    s = requests.Session()
-
-    url = "http://g6.fuse.tikal.io/cart"
-    body = {
-        "id": "03fef6ac-1896-4ce8-bd69-b798f85c6e0b"
-    }
-    request_body = json.dumps(body)
-    headers = {'Content-Type': 'application/json; charset=UTF-8'}
-    print(request_body)
-    post_response = s.post(url=url, data=request_body, headers=headers)
-    print("Add to cart %s" % post_response)
-
-    params = {}
-
-    r = s.get(url=url, params=params)
-
-    data = r.json()
-
-    # printing the output
-    print("Cart get response: %s" % str(data))
+    # s = requests.Session()
+    # cart_url = "http://g6.fuse.tikal.io/cart"
+    #
+    # request_body = {
+    #     "id": "03fef6ac-1896-4ce8-bd69-b798f85c6e0b"
+    # }
+    # request_body_str = json.dumps(request_body)
+    # headers = {'Content-Type': 'application/json; charset=UTF-8'}
+    # print(request_body)
+    # post_response = s.post(url=cart_url, data=request_body_str, headers=headers)
+    # print("Add to cart %s" % post_response)
+    #
+    # r = s.get(url=cart_url)
+    #
+    # data = r.json()
+    #
+    # # printing the output
+    # print("Cart get response: %s" % str(data))
+    print("Running cart attack...")
+    os.system("./run_cart_attack.sh")
+    print("Running cart attack...")
---
+++ server/run_cart_attack.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+loadtest -k http://g6.fuse.tikal.io/cart -m GET --rps 50 -n 2000
